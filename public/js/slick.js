eea9261efd63b8c6b8c5cea6a547d7a4819102f7
Caitlin Everett <caitlineverett@gmail.com>
2018-03-18 10:39:57 -0500

    profile page basic bones built in

--- public/js/slick.js
+++ public/js/slick.js
@@ -23,28 +23,27 @@
     ]
   });
 
-  //   $('.center').slick({
-  //   centerMode: true,
-  //   centerPadding: '60px',
-  //   slidesToShow: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 3
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //   ]
-  // });
+  $('#freezer').slick({
+    centerMode: false,
+    slidesToShow: 7,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
