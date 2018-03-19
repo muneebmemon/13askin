a16c5bc866e81f5c7790a365ef7211d9433d58c3
Caitlin Everett <caitlineverett@gmail.com>
2018-03-19 02:03:59 -0500

    added lots of html and css

--- public/js/slick.js
+++ public/js/slick.js
@@ -8,7 +8,7 @@
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 1
         }
       },
       {
@@ -25,13 +25,18 @@
 
   $('#freezer').slick({
     centerMode: false,
-    slidesToShow: 7,
+    slidesToShow: 6,
     responsive: [{
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          slidesToShow: 5
+        }
+      },
+      {
         breakpoint: 768,
         settings: {
           arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 3
         }
       },
@@ -39,11 +44,8 @@
         breakpoint: 480,
         settings: {
           arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 1
         }
       }
     ]
   });
-
